4823d5514efc5c5625c73ab538edfd4073b1abec
sumanareddy <s538360@nwmissouri.edu>
2021-03-01 09:12:09 -0500

    added location.js

---
+++ location.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        name: '1213 Apartments',
+        type: 'quad',
+        coordinates: [
+            {
+                latitude: 40.360857,
+                longitude: -94.888152
+            },
+            {
+                latitude: 40.360849,
+                longitude: -94.887489
+            },
+            {
+                latitude: 40.360495,
+                longitude: -94.887500
+            },
+            {
+                latitude: 40.360521,
+                longitude: -94.888210
+            }
+        ]
+    },
+    
+    {
+        Name:'B.D Owens Library',
+        Latitude:40.35385, 
+        Longitude:-94.88591,
+    }
+
+];
