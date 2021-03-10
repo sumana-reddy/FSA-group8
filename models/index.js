3438e5d3a224a2552f37b239e8c0fef082379868
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2021-03-09 18:57:00 -0600

    create model

---
+++ models/index.js
@@ -0,0 +1,72 @@
+/**
+ *  Model index.js - adds all model definitions into sequelize
+ *  Updated process all files in the models folder
+ *  rather than explicitly calling require for each
+ *
+ *
+ */
+const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
+const { Sequelize, DataTypes } = require('sequelize');
+
+const envConfigs = require('../config/config');
+const LOG = require('../util/logger');
+const applyExtraSetup = require('./index-setup'); // add associations
+
+module.exports = async () => {
+  LOG.info('Starting models/index.js .......................');
+
+  /**
+   * Connect and initialize the database.
+   */
+  const dbInit = async () => {
+    /**
+     * Load environment variables from .env file,
+     *  where API keys and passwords can be configured.
+     */
+    const vars = dotenv.config({ path: '.env' });
+    if (vars.error) {
+      throw vars.error;
+    }
+    LOG.info(`MODELS/INDEX: Environment variables loaded: ${vars.parsed}`);
+
+    const basename = path.basename(__filename);
+    const isProduction = process.env.NODE_ENV === 'production';
+    LOG.info(`MODELS/INDEX: In ${process.env.NODE_ENV} environment.`);
+
+    const config = envConfigs[process.env.NODE_ENV];
+
+    const sequelize = isProduction
+      ? new Sequelize(config.url, config)
+      : new Sequelize(config);
+
+    LOG.info('MODELS/INDEX: created Sequelize connection.');
+
+    fs.readdirSync(__dirname)
+      .filter((file) => {
+        return (
+          file.indexOf('.') !== 0 &&
+          file !== basename &&
+          file.slice(-3) === '.js'
+        );
+      })
+      .forEach((file) => {
+        // we like require statement to be explicit - and at the top
+        // of the file, but this is more concise, so for now at
+        // least we are breaking the rules
+        //
+        // require each model definer
+        require(path.join(__dirname, file))(sequelize, DataTypes);
+      });
+
+    LOG.info('MODELS/INDEX: Added model definers.');
+
+    // Apply extra setup after model definition (e.g. associations).
+    await applyExtraSetup(sequelize);
+
+    return sequelize;
+  };
+
+  return dbInit();
+};
---
+++ models/location.js
@@ -0,0 +1,41 @@
+/**
+ *  Location model
+ *  Describes the characteristics of each attribute in a Location resource.
+ *
+ *
+ * For more information about defining sequelize models, see
+ * https://sequelize.org/v5/manual/data-types.html
+ * https://sequelize.org/master/manual/validations-and-constraints.html
+ *
+ * For validators see: https://github.com/validatorjs/validator.js
+ *
+ */
+// Export a function that defines the model.
+// It automatically receives the Sequelize connection parameter.
+
+module.exports = (db, DataTypes) => {
+  db.define('Location', {
+    locationId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    locationLatitude: {
+      type: DataTypes.DECIMAL(10, 8),
+      allowNull: false
+    },
+    locationLongitude: {
+      type: DataTypes.DECIMAL(11, 8),
+      allowNull: false
+    },
+
+    locationValue: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+
+
+
+  });
+ 
+};
