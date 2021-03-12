e85ae6d62d11bb05c0ba2e360bf0c1a9b2ba12fa
sumana-reddy <60016064+sumana-reddy@users.noreply.github.com>
2021-03-12 09:49:45 -0500

    added new locations

--- location.js
+++ location.js
@@ -1,5 +1,91 @@
 export default [
     {
+        name: 'Colden Pond' ,
+        type: 'quad',
+        coordinates: [
+            
+            {
+               latutide: 40.35112686830798, 
+               longitude: -94.88220243759864
+            },
+                        
+            {            
+              latitude: 40.35116775003509, 
+              longitude: -94.88298564260305            
+            },            
+            
+            {
+                latitude: 40.35096334115174, 
+                longitude: -94.88301782911009
+            },
+            
+            {
+                 latitude: 40.35066899127124, 
+                 longitude: -94.88243847198352
+            }
+            
+            ]
+    },
+    
+    {
+        name: 'Horizons West apartments',
+        type:'quad',
+        coordinates: [
+            
+            {
+                latitude: 40.35610101337432, 
+                longitude: -94.88257240610433                
+            },
+            
+            {
+                latitude: 40.355882839646014, 
+                longitude: -94.88257240610433
+            },
+            
+            {
+                latitude: 40.35613862946302, 
+                longitude: -94.8814173186114
+            },
+            
+            {
+                latitude: 40.355807607162234, 
+                longitude: -94.88142719115409
+            },
+            
+            ]
+        
+    },
+    
+    {
+        name: 'B.D.Owens Library',
+        type: 'quad',
+        coordinates: [
+            {
+                latitude: 40.35376272456885, 
+                longitude: -94.88561589359217
+            },
+            
+            {
+                latitude: 40.35375018511063, 
+                longitude: -94.88654558193885
+            },
+            
+            {
+                latitude: 40.353374000279494, 
+               longitude: -94.88636458066782
+            },
+            
+            {
+                latitude: 40.35342415837825, 
+                longitude: -94.88566525757518
+            },
+            
+            ]
+        
+        
+    },
+    
+    {
         name: '1213 Apartments',
         type: 'quad',
         coordinates: [
