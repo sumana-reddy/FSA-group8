2c0c646501c0a50ccc20a412eaf358595711322d
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2021-03-12 00:08:16 -0600

    modified routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const locationRoutes = require('./location.routes');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-try {
-  router.use('/location', locationRoutes);
-} catch (err) {
-  LOG.error(`ERROR: ${err.message}`);
-}
+
 module.exports = router;
--- scripts/speech.js
+++
@@ -1,4 +0,0 @@
-export default [
-    {status_message : "This is color Brown."},
-    {status1_message : "This is color Orange."}
-]
