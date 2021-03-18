62b9c5636029d39cb179978cdbf6d260a90407ed
sumana-reddy <60016064+sumana-reddy@users.noreply.github.com>
2021-03-18 18:22:56 -0400

    added new router file

---
+++ routes/delete.js
@@ -0,0 +1,13 @@
+/**
+ * Route team requests to the correct controller function.
+ *
+ * A request includes:
+ *  an HTTP verb (e.g., get or post) AND
+ *  a URL endpoint (e.g., /create)
+ *
+ * Match each expeced verb + URL request
+ * with a custom function to handle it
+ *
+ * @author Sumana Reddy <s538360@nwmissouri.edu>
+ */
+
