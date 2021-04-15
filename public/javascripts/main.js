08e68c31c684dfdca5e8d7723fc9a02e1a839ffb
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2021-04-15 17:20:10 -0500

    Updated the distance from current position

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -95,8 +95,6 @@ async function onClickSquareBox1() {
                 });
             }
 
-        console.log(myArrayElement);
-
             location = myArrayElement;
             secondBoxFlag = true;
         } else {
@@ -121,7 +119,57 @@ async function onClickSquareBox1() {
 
 }
 
+function getMinimumDistanceFromCurrentLocation() {
+    return Math.min(getDistance( 0), getDistance( 1), getDistance( 2), getDistance( 3)).toFixed(2);
+}
 
+//Get the distance from a point
+let getDistance = function(i) {
+    let coordinates = location.coordinates;
+    let locationLat = coordinates[i]["latitude"];
+    let locationLon = coordinates[i]["longitude"];
+    console.log(locationLat + ":" + locationLon);
+    let R = 6378137; // Earth’s mean radius in meter
+    let dLat = rad(device.coords.latitude - locationLat);
+    let dLong = rad(device.coords.longitude - locationLon);
+    let a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(rad(locationLat)) * Math.cos(rad(device.coords.latitude)) *
+        Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    let d = R * c;
+    return d; // returns the distance in meter
+};
+
+let rad = function(x) {
+    return x * Math.PI / 180;
+
+};
+
+//Get the directions to the current device location
+export function getLocationDirections() {
+    let directionArray = [];
+    let [x, y] = [device.coords.latitude, device.coords.longitude];
+
+    const latitudes = location.coordinates
+        .map((item) => item.latitude)
+        .sort((a, b) => b - a);
+    const longitudes = location.coordinates
+        .map((item) => item.longitude)
+        .sort((a, b) => a - b);
+
+    console.log("Latitudes: " + latitudes);
+    console.log("Longitudes: " + longitudes);
+
+    if (x > latitudes[0] || x > latitudes[1]) directionArray.push("South");
+    else if (x < latitudes[2] || x < latitudes[3])
+        directionArray.push("North");
+
+    if (y < longitudes[0] || y < longitudes[1]) directionArray.push("East");
+    else if (y > longitudes[2] || y > longitudes[3])
+        directionArray.push("West");
+
+    return directionArray;
+}
 
 async function onClickSquareBox2() {
     if (secondBoxFlag) {
@@ -144,9 +192,9 @@ async function onClickSquareBox2() {
                     }
                 })
             } else {
-                status += "You haven't reached the quest"
-                let distanceBetween = getDistanceFromCurrentLocation(location, device);
-                speak = "You haven't reached the quest";
+                let distanceBetween = getMinimumDistanceFromCurrentLocation();
+                status += "You haven't reached the quest. You're " + distanceBetween + " meters " + getLocationDirections() + " to the quest.";
+                speak = "You haven't reached the quest. You're " + distanceBetween + " meters " + getLocationDirections() + " to the quest.";
             }
 
 
